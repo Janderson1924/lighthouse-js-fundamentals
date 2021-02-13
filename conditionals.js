57c9d29e692704850e1e9a77c42bb00583c9a73f
Justin <justinanderson101@gmail.com>
2021-02-13 22:00:16 +0000

    conditional execution

--- conditionals.js
+++ conditionals.js
@@ -1,7 +1,40 @@
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+
+// New Exercise
+
+
 const isCitizen = true;
 const age = 26;
 
 if (isCitizen && age > 18) {
   console.log("You are eligible to vote.");
 }
+
+
+// New exercise
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+
+// New exericse
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
+
 
