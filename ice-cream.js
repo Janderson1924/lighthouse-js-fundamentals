49e0c60ffc94a28bfd74ca4973dd421d168d3d1e
Justin <justinanderson101@gmail.com>
2021-02-17 22:44:50 +0000

    added comments for clarity

--- ice-cream.js
+++ ice-cream.js
@@ -1,13 +1,13 @@
 const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
 
-console.log(iceCreamFlavours);
+console.log(iceCreamFlavours); // prints all flavours
 
-iceCreamFlavours.push('root beer');
+iceCreamFlavours.push('root beer'); // add root beer to end 
 
-console.log(iceCreamFlavours);
+console.log(iceCreamFlavours); // prints all flavours
 
-console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[0]); // prints flavour in index 0 spot
 
-console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours[5]); // prints flavour in index 5 spot
 
-console.log(iceCreamFlavours.length);
+console.log(iceCreamFlavours.length); // prints the legnth of the array
