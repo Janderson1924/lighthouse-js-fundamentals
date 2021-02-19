2914712e73e8812e4c09560c15513ea528d3b5c3
Justin <justinanderson101@gmail.com>
2021-02-19 00:57:20 +0000

    rooting for you problem

---
+++ rooting.js
@@ -0,0 +1,26 @@
+const judgeVegetable = function (vegetables, metric) {
+  vegetables.sort((x, y) => y[metric] - x[metric]);
+  return vegetables[0].submitter;
+}
+
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  }
+]
+
+const metric = 'redness'
+
+console.log(judgeVegetable(vegetables, metric));
